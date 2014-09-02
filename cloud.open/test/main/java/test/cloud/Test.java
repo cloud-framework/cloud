21462aa902d349e8115c1602d36d385132c4ac91
wendellup <wendellup@163.com>
2014-09-02 20:56:45 +0800

    测试方法修改

--- cloud.open/test/main/java/test/cloud/Test.java
+++ cloud.open/test/main/java/test/cloud/Test.java
@@ -5,28 +5,36 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import cn.egame.common.util.Utils;
+
 public class Test {
 	public static void main(String[] args) {
+		System.getProperties().setProperty("proxySet", "true");
+        System.getProperties().setProperty("http.proxyHost", "49.213.20.171");
+        System.getProperties().setProperty("http.proxyPort", "8080");
+		
 		Document detailDoc = null;
 		String url = "https://www.google.com/cse?cx=006369457434482665413:mnzddbuz2ke&ie=UTF-8&q=&ref=#gsc.q=%E9%A9%AF%E9%BE%99%E9%AB%98%E6%89%8B2";
 		String url2 = "http://torrentproject.com/?t=%E9%A9%AF%E9%BE%99%E9%AB%98%E6%89%8B2";
-		try {
-			detailDoc = Jsoup.connect(url2)
-					.timeout(30000)
-			// .header("User-Agent",
-			// UserAgentAry[Utils.getRandom(UserAgentAry.length)])
-			// .header("Accept",
-			// "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-			// .header("Accept-Encoding", "gzip,deflate,sdch")
-			// .header("Accept-Language",
-			// "en-US,en;q=0.8,ja;q=0.6,zh-CN;q=0.4,zh;q=0.2")
-			// .header("Connection", "keep-alive")
-			// .header("Cache-Control", "max-age=0")
-			// .header("Cookie", "bid=\""+SpiderUtils.getRandomStr(11)+"\"; ")
-					.get();
-		} catch (IOException e) {
-			e.printStackTrace();
+		for(int i=0; i<10; i++){
+			try {
+				detailDoc = Jsoup.connect(url2)
+						.timeout(10000)
+//						 .header("User-Agent",
+//						 UserAgentAry[Utils.getRandom(UserAgentAry.length)])
+						 .header("Accept",
+						 "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+						 .header("Accept-Encoding", "gzip,deflate,sdch")
+						 .header("Accept-Language",
+						 "en-US,en;q=0.8,ja;q=0.6,zh-CN;q=0.4,zh;q=0.2")
+						 .header("Connection", "keep-alive")
+						 .header("Cache-Control", "max-age=0")
+						// .header("Cookie", "bid=\""+SpiderUtils.getRandomStr(11)+"\"; ")
+						.get();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println(detailDoc.toString());
 		}
-		System.out.println(detailDoc.toString());
 	}
 }
